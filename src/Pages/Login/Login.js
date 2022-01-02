3ae7a6271830cde2fc30591d720449e67e925037
Aal Emraan <developer.aalemraan@gmail.com>
2022-01-02 16:19:26 +0600

    updated

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,19 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 const Login = () => {
-  const [products, setProducts] = useState();
-
-  useEffect(() => {
-    fetch("./products.json")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
-
-  console.log(products);
   return (
     <div>
       <h1>this is login</h1>
-      <p>{products?.length}</p>
     </div>
   );
 };
