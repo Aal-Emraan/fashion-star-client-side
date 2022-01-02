f602ffac1dbecd5bafab376a9f41d10495466e76
Naimur53 <naimurrhman53@gmail.com>
2022-01-02 22:20:38 +0600

    done

--- src/Pages/Glasses/Glasses/Glasses.js
+++ src/Pages/Glasses/Glasses/Glasses.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import GlassCard from '../GlassCard/GlassCard'
+import { useDispatch, useSelector } from "react-redux";
+import { Container, Grid } from '@mui/material'
+import { getWatches, selectData, setLoading } from "../../../dataSlice/dataSlice";
 
 const Glasses = () => {
+  const dispatch = useDispatch();
+  const data = useSelector(selectData)
+
+  useEffect(() => {
+    dispatch(getWatches())
+    console.log('sd');
+    dispatch(setLoading(true))
+  }, [])
+  console.log(data);
   return (
-    <div className="min-h-screen bg-red-500">
-      <h1>this is glasses</h1>
+    <div className="min-h-screen ">
+      <div className="mt-5 py-5">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, debitis! Voluptates assumenda cupiditate ex est tempore illum dignissimos id porro minima nostrum, alias ut repudiandae ducimus quidem eos vero et?
+
+      </div>
+      <Container>
+        <Grid container spacing={4}>
+          {
+            data.glasses.map(glass => <GlassCard data={glass}></GlassCard>)
+          }
+        </Grid>
+      </Container>
+
     </div>
   );
 };
