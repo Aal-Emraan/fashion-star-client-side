13d82d06bc8426d81e4ace52c6a68073dacb270d
Minhajul Galib <62963105+Minhajul-galib@users.noreply.github.com>
2022-01-04 00:44:21 +0600

    8th client

--- src/Pages/ProductDetails/Details.js
+++ src/Pages/ProductDetails/Details.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
+import useProducts from '../../Hooks/useProducts';
 import './Details.css'
+import RelatedDetails from './RelatedDetails/RelatedDetails';
 const Details = () => {
 
     const { id } = useParams();
     const [details, setDetails] = useState({});
-    
+    const [products] = useProducts();
+
+    console.log(id);
+    const selectedProducts = products.filter(product => product.category === details?.category);
+    const selectedProducts2 = selectedProducts.filter(selectedProduct => selectedProduct._id !== id);
+    const productSlice = selectedProducts2.slice(0, 4)
+
     useEffect(()=>{
         const url = `http://localhost:5000/products/${id}`;
 
@@ -16,18 +24,18 @@ const Details = () => {
         })
     }, [id]);
     
-    console.log(details);
     const description = details?.description;
-    // const shortDescription = description.slice(0, 200);
+    const shortDescription = description?.slice(0, 200);
+
     return (
         <div style={{minHeight: '80vh'}}>
             <div className="details">
                 <div className='details-header'>
-                <h2>ok</h2>
+                <h2>{details?.title}</h2>
                 </div>
                 <div className="main-details flex gap-4">
                     <div style={{width:'50%'}}>
-                        <img src={details.img} alt="" />
+                        <img style={{width: '550px'}} src={details.img} alt="" />
                     </div>
                     <div className='details-content' style={{width:'50%'}}>
                         <h4>{details?.title}</h4>
@@ -37,20 +45,33 @@ const Details = () => {
                         </div>
 
                         <p>{details?.average_rating} <span>({details?.total_rating})</span></p>
-                        {/* <p className='short-des'>{shortDescription} ...</p> */}
-                        <p className='short-des'>{description} ...</p>
+                        <p className='short-des'>{shortDescription} ...</p>
                         
-                        <div className='product-count flex gap-5'>
+                        <div className='product-count flex gap-5 mb-7'>
                             <input type="number" />
                             <button>ADD TO CART</button>
+                            <button>CHECKOUT</button>
                         </div>
                         
-                        <h5>FOR: <span>{details?.for}</span></h5>
-                        <h5>CATEGORY: <span>{details?.category}</span></h5>
+                        <h5 className='details-category'>FOR: <span className='details-span'>{details?.for}</span></h5>
+                        <h5 className='details-category'>CATEGORY: <span className='details-span'>{details?.category}</span></h5>
 
                         <div>
-                            <h5>{details?.available_colors}</h5>
+                            {/* <h5>{details?.available_colors}</h5> */}
                         </div>
+                    </div>
+                </div>
+
+                <div className='relate-products'>
+                <h2>Related Products</h2>
+                    <div className='related-product-div'>
+                    
+                    {
+                    productSlice.map(product => <RelatedDetails
+                                                            key={product._id}
+                                                            product={product}
+                                                            ></RelatedDetails>)
+                    }
                     </div>
                 </div>
             </div>
