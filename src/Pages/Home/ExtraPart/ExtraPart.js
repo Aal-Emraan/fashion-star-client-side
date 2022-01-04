ed3a88b80a8b80d1cbb842b166b49cbf36765184
ijsathi <ijsathi2001rs@gmail.com>
2022-01-04 18:05:49 +0600

    Extra Part Added

--- src/Pages/Home/ExtraPart/ExtraPart.js
+++ src/Pages/Home/ExtraPart/ExtraPart.js
@@ -1,20 +1,335 @@
 import { Box, Grid, Typography } from '@mui/material';
 import React from 'react';
+import './ExtraPart.css';
 
 const ExtraPart = () => {
     return (
         <>
-            <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={1}>
+            <Box style={{padding:"30px"}} sx={{ flexGrow: 1 }}>
+                <Grid container>
                     <Grid item xs={6}>
-                        <img src="https://cdn.shopify.com/s/files/1/0250/5169/3134/files/img-bottom-banner_540x.jpg?v=1563081470" alt="" />
+                        <img src="https://m.media-amazon.com/images/I/81Z1ZpsAWrL._AC_SL1500_.jpg" alt="" />
                     </Grid>
                     <Grid item xs>
-                        <Typography style={{ fontWeight: "bold" }} variant='h5'>
+                        <Typography style={{ fontWeight: "bold", color:"rgb(58, 57, 56)" }} variant='h5'>
                             Best seller product
                         </Typography>
-                        <Grid item xs>
-                            <img width='60px' src="https://cdn.shopify.com/s/files/1/0250/5169/3134/files/img-bottom-banner_540x.jpg?v=1563081470" alt="" />
+                        <Grid className="g"  >
+                            <Grid item xs>
+                                <img width='80px' src="https://5.imimg.com/data5/OT/CH/MY-5240844/promotional-sunglasses-500x500.jpg" alt="" />
+                            </Grid>
+                            <Grid className="g2" item xs>
+                                <Typography style={{ }} variant='p'>
+                                    1. Sun Glasses
+                                </Typography><br />
+                                <Grid className='pri'>
+                                <Typography
+                                    style={{  }}
+                                    className="price1"
+                                    variant="p"
+                                >
+                                    $130.00
+                                </Typography><br />
+                                <Typography
+                                    className="price2"
+                                    style={{ }}
+                                    variant="p"
+                                >
+                                    $110.00
+                                </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid className="g"  >
+                            <Grid item xs>
+                                <img width='80px' src="https://cdn.shopify.com/s/files/1/0250/5169/3134/products/15_91120dec-2472-4b82-84b6-1dd95f82407c_180x.jpg?v=1567661951" alt="" />
+                            </Grid>
+                            <Grid className="g2" item xs>
+                                <Typography style={{ }} variant='p'>
+                                    2. Diamond Ring
+                                </Typography><br />
+                                <Grid className='pri'>
+                                <Typography
+                                    style={{  }}
+                                    className="price1"
+                                    variant="p"
+                                >
+                                    $490.00
+                                </Typography><br />
+                                <Typography
+                                    className="price2"
+                                    style={{ }}
+                                    variant="p"
+                                >
+                                    $489.00
+                                </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid className="g"  >
+                            <Grid item xs>
+                                <img width='80px' src="https://cdn.shopify.com/s/files/1/0250/5169/3134/products/9_0f913846-4ed3-414b-b037-cdbdcad3a7fe_180x.jpg?v=1567661657" alt="" />
+                            </Grid>
+                            <Grid className="g2" item xs>
+                                <Typography style={{ }} variant='p'>
+                                    3. Diamond Pin
+                                </Typography><br />
+                                <Grid className='pri'>
+                                <Typography
+                                    style={{  }}
+                                    className="price1"
+                                    variant="p"
+                                >
+                                    $340.00
+                                </Typography><br />
+                                <Typography
+                                    className="price2"
+                                    style={{ }}
+                                    variant="p"
+                                >
+                                    $335.00
+                                </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid className="g"  >
+                            <Grid item xs>
+                                <img width='80px' src="https://i.ibb.co/7YmZQjW/the-best-is-yet-to-come-framed-poster.jpg" alt="" />
+                            </Grid>
+                            <Grid className="g2" item xs>
+                                <Typography style={{ }} variant='p'>
+                                    4. Iron man Anti blue
+                                </Typography><br />
+                                <Grid className='pri'>
+                                <Typography
+                                    style={{  }}
+                                    className="price1"
+                                    variant="p"
+                                >
+                                    $74.00
+                                </Typography><br />
+                                <Typography
+                                    className="price2"
+                                    style={{ }}
+                                    variant="p"
+                                >
+                                    $69.00
+                                </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid className="g"  >
+                            <Grid item xs>
+                                <img width='80px' src="https://i.imgur.com/KK2t1mJ.png" alt="" />
+                            </Grid>
+                            <Grid className="g2" item xs>
+                                <Typography style={{ }} variant='p'>
+                                    5. Golden Gucci Women`s Watch
+                                </Typography><br />
+                                <Grid className='pri'>
+                                <Typography
+                                    style={{  }}
+                                    className="price1"
+                                    variant="p"
+                                >
+                                    $290.00
+                                </Typography><br />
+                                <Typography
+                                    className="price2"
+                                    style={{ }}
+                                    variant="p"
+                                >
+                                    $269.80
+                                </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid className="g"  >
+                            <Grid item xs>
+                                <img width='80px' src="https://i.imgur.com/eywvpo5.png" alt="" />
+                            </Grid>
+                            <Grid className="g2" item xs>
+                                <Typography style={{ }} variant='p'>
+                                    6. Deep Blue Men`s Watch
+                                </Typography><br />
+                                <Grid className='pri'>
+                                <Typography
+                                    style={{  }}
+                                    className="price1"
+                                    variant="p"
+                                >
+                                    $55.00
+                                </Typography><br />
+                                <Typography
+                                    className="price2"
+                                    style={{ }}
+                                    variant="p"
+                                >
+                                    $50.00
+                                </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs>
+                        <Typography style={{ fontWeight: "bold", color:"rgb(58, 57, 56)" }} variant='h5'>
+                        On-sale product
+                        </Typography>
+                        <Grid className="g"  >
+                            <Grid item xs>
+                                <img width='80px' src="https://cdn.shopify.com/s/files/1/0250/5169/3134/products/1_180x.jpg?v=1567660859" alt="" />
+                            </Grid>
+                            <Grid className="g2" item xs>
+                                <Typography style={{ }} variant='p'>
+                                    1. Earrings
+                                </Typography><br />
+                                <Grid className='pri'>
+                                <Typography
+                                    style={{  }}
+                                    className="price1"
+                                    variant="p"
+                                >
+                                    $130.00
+                                </Typography><br />
+                                <Typography
+                                    className="price2"
+                                    style={{ }}
+                                    variant="p"
+                                >
+                                    $110.00
+                                </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid className="g"  >
+                            <Grid item xs>
+                                <img width='80px' src="https://n4.sdlcdn.com/imgs/i/z/i/HMTe-HM-9072-Metal-Analog-SDL494827480-1-db8fc.jpg" alt="" />
+                            </Grid>
+                            <Grid className="g2" item xs>
+                                <Typography style={{ }} variant='p'>
+                                    2. Meta analog Watch
+                                </Typography><br />
+                                <Grid className='pri'>
+                                <Typography
+                                    style={{  }}
+                                    className="price1"
+                                    variant="p"
+                                >
+                                    $350.00
+                                </Typography><br />
+                                <Typography
+                                    className="price2"
+                                    style={{ }}
+                                    variant="p"
+                                >
+                                    $349.99
+                                </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid className="g"  >
+                            <Grid item xs>
+                                <img width='80px' src="https://www.orra.co.in/pub/media/catalog/product/o/s/osn19120-a.jpg" alt="" />
+                            </Grid>
+                            <Grid className="g2" item xs>
+                                <Typography style={{ }} variant='p'>
+                                    3. Neckless
+                                </Typography><br />
+                                <Grid className='pri'>
+                                <Typography
+                                    style={{  }}
+                                    className="price1"
+                                    variant="p"
+                                >
+                                    $790.00
+                                </Typography><br />
+                                <Typography
+                                    className="price2"
+                                    style={{ }}
+                                    variant="p"
+                                >
+                                    $780.00
+                                </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid className="g"  >
+                            <Grid item xs>
+                                <img width='80px' src="https://5.imimg.com/data5/EN/JJ/MY-11896129/stylish-mirror-sunglasses-250x250.jpg" alt="" />
+                            </Grid>
+                            <Grid className="g2" item xs>
+                                <Typography style={{ }} variant='p'>
+                                    4. Stylish Mirror Sunglass
+                                </Typography><br />
+                                <Grid className='pri'>
+                                <Typography
+                                    style={{  }}
+                                    className="price1"
+                                    variant="p"
+                                >
+                                    $130.00
+                                </Typography><br />
+                                <Typography
+                                    className="price2"
+                                    style={{ }}
+                                    variant="p"
+                                >
+                                    $110.00
+                                </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid className="g"  >
+                            <Grid item xs>
+                                <img width='80px' src="https://cdn.shopify.com/s/files/1/0250/5169/3134/products/5_350138cb-482f-4a40-b11e-7dda7173acbd_180x.jpg?v=1616674192" alt="" />
+                            </Grid>
+                            <Grid className="g2" item xs>
+                                <Typography style={{ }} variant='p'>
+                                    5. Bracelet
+                                </Typography><br />
+                                <Grid className='pri'>
+                                <Typography
+                                    style={{  }}
+                                    className="price1"
+                                    variant="p"
+                                >
+                                    $80.00
+                                </Typography><br />
+                                <Typography
+                                    className="price2"
+                                    style={{ }}
+                                    variant="p"
+                                >
+                                    $79.00
+                                </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid className="g"  >
+                            <Grid item xs>
+                                <img width='80px' src="https://atlas-content-cdn.pixelsquid.com/stock-images/gold-sunglasses-z0nOn23-600.jpg" alt="" />
+                            </Grid>
+                            <Grid className="g2" item xs>
+                                <Typography style={{ }} variant='p'>
+                                    6. Gold Sunglasses
+                                </Typography><br />
+                                <Grid className='pri'>
+                                <Typography
+                                    style={{  }}
+                                    className="price1"
+                                    variant="p"
+                                >
+                                    $310.00
+                                </Typography><br />
+                                <Typography
+                                    className="price2"
+                                    style={{ }}
+                                    variant="p"
+                                >
+                                    $299.99
+                                </Typography>
+                                </Grid>
+                            </Grid>
                         </Grid>
                     </Grid>
                 </Grid>
